12bec3f0cbff334a1b457676f07c0fe4ea28897b
aaron-burnham17 <argust34@gmail.com>
2018-05-10 13:58:00 -0700

    first two functions are working. could probably be defined more succintly

--- src/main/java/edu/gcccd/csis/MyProject2.java
+++ src/main/java/edu/gcccd/csis/MyProject2.java
@@ -7,13 +7,51 @@ public class MyProject2 implements Project2 {
 
     @Override
     public NodeList<Integer> addition(NodeList<Integer> nodeList1, NodeList<Integer> nodeList2) {
+        NodeList<Integer> Total = new NodeList<Integer>();
+        NodeList<Integer> Remainder = new NodeList<Integer>();
+        boolean noRemainder = true;
+        int append = 0;
         do {
-
-
-            while (nodeList1.iterator().next() + nodeList2.iterator().next() >= 9) {
+            if(nodeList1.getLength() > nodeList2.getLength()){
+                Total.append(nodeList1.iterator().next());
+                nodeList1.remove(nodeList1.iterator().next());
+                Remainder.append(0);
 
             }
-        }while(true);
+            else if(nodeList2.getLength() > nodeList1.getLength()){
+                Total.append(nodeList2.iterator().next());
+                nodeList2.remove(nodeList2.iterator().next());
+                Remainder.append(0);
+
+            }
+
+            if(nodeList1.getLength() == nodeList2.getLength()){
+                append = nodeList1.iterator().next() + nodeList2.iterator().next();
+                nodeList1.remove(nodeList1.iterator().next());
+                nodeList2.remove(nodeList2.iterator().next());
+                if(append >= 10){
+                    append %= 10;
+                    Remainder.append(1);
+                    noRemainder = false;
+                }
+                else{
+                    Remainder.append(0);
+                }
+                Total.append(append);
+            }
+
+
+        }while(nodeList1.iterator().hasNext() && nodeList2.iterator().hasNext());
+        Remainder.append(0);
+        if (noRemainder){
+            while(Total.iterator().next() == 0){
+                Total.remove(0);
+            }
+            return Total;
+        }
+        else{
+            return addition(Total,Remainder);
+        }
     }
 
     @Override
@@ -47,13 +85,12 @@ public class MyProject2 implements Project2 {
     @Override
     public NodeList<Integer> load(String fileName) {
         NodeList list = new NodeList();
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        String c = "";
-
         try {
-            while ((c = br.read()) != null) {
-                int i = Integer.parseInt(c);
-                list.append(i);
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            int c;
+            while ((br.read() != (int)(' '))) {
+                c = br.read();
+                list.append(c);
             }
 
             br.close();
@@ -78,10 +115,11 @@ public class MyProject2 implements Project2 {
         Project2.print(p.addition(n1, n2)); //  n1+n2, e.g. 4139
 
         final NodeList<NodeList<Integer>> listOfLists = new NodeList<>();
-        for (int i = 0; i < L; i++) {
-            listOfLists.append(Project2.generateNumber(L));
+        for (int i = 0; i < 5; i++) {
+            listOfLists.append(Project2.generateNumber(5));
         }
-        p.save(p.addition(listOfLists.iterator()), "result.bin");
-        Project2.print(p.load("result.bin"));
+        //Iterator<NodeList<Integer>> listAdditive = listOfLists;
+//        p.save(p.addition(listOfLists.iterator()), "result.bin");
+//        Project2.print(p.load("result.bin"));
     }
 }
